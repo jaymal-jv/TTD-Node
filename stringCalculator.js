7c16212a4bd02413f189674123b667d844b1c247
jaymal-jv <odedarajaymal52@gmail.com>
2025-05-20 08:02:36 +0530

    Implement add method to handle new line separators

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,8 @@
 class StringCalculator {
     add(input) {
       if (input === '') return 0;
-      const numbers = input.split(',').map(num => parseInt(num, 10));
+      const delimiters = [',', '\n'];
+      const numbers = input.split(new RegExp(`[${delimiters.join('')}]`)).map(num => parseInt(num, 10));
       return numbers.reduce((sum, num) => sum + num, 0);
     }
   }
