0fb33eace675ca10ffbb69807bc609b636e32051
jaymal-jv <odedarajaymal52@gmail.com>
2025-05-20 07:57:47 +0530

    Implement add method for two numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,9 @@
 class StringCalculator {
     add(input) {
       if (input === '') return 0;
-      return parseInt(input, 10);
+      if (!input.includes(',')) return parseInt(input, 10);
+      const numbers = input.split(',').map(num => parseInt(num, 10));
+      return numbers.reduce((sum, num) => sum + num, 0);
     }
   }
   
