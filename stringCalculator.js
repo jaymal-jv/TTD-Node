75741286651f78307d75a719ef1a84d6c7f08795
jaymal-jv <odedarajaymal52@gmail.com>
2025-05-20 08:05:31 +0530

    Implement add method to throw error for negative numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,6 +3,12 @@ class StringCalculator {
       if (input === '') return 0;
       const delimiters = [',', '\n'];
       const numbers = input.split(new RegExp(`[${delimiters.join('')}]`)).map(num => parseInt(num, 10));
+      
+      const negatives = numbers.filter(num => !isNaN(num) && num < 0);
+      if (negatives.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negatives.join(', ')}`);
+      }
+  
       return numbers.reduce((sum, num) => sum + (isNaN(num) ? 0 : num), 0);
     }
   }
